3b7096467b2a2a55c4fb279532728ed0e406c02c
Amruta Dharmapurikar <23amrutad@Amrutas-MacBook-Pro.local>
2021-02-24 17:10:06 -0800

    prevents the testing and extension from running into each other

--- News_Extraction_Extension/content.js
+++ News_Extraction_Extension/content.js
@@ -1,15 +1,16 @@
-if (window.location.hostname != '127.0.0.1') {
-    // so that my testing and the extension don't run into each other
-    console.log(window.location.hostname)
-    var text = document.body.innerText;
-    //console.log(text);
-    const Http = new XMLHttpRequest();
-    const url = 'http://127.0.0.1:20000/summarize_text?text=' + text;
-    Http.open("GET", url);
-    Http.send();
-    Http.onreadystatechange = (e) => {
-        console.log(Http.responseText)
+function content_script() {
+    if (window.location.hostname != '127.0.0.1') {
+        // so that my testing and the extension don't run into each other
+        console.log(window.location.hostname)
+        var text = document.body.innerText;
+        //console.log(text);
+        const Http = new XMLHttpRequest();
+        const url = 'http://127.0.0.1:20000/summarize_text?text=' + text;
+        Http.open("GET", url);
+        Http.send();
+        Http.onreadystatechange = (e) => {
+            console.log(Http.responseText)
+        }
     }
 }
-
-//alert("Hello from your Chrome extension!")
+setTimeout(content_script(), 1000)
